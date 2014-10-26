3baf0c7a6569c6fee84c49bdc03e19ee9a510aa1
Kelvin Nicholson <kelvin@kelvinism.com>
2014-10-26 16:12:18 +1100

    Updated tests

--- tests/lifeSpec.js
+++ tests/lifeSpec.js
@@ -1,7 +1,11 @@
 describe("The 'lifeexpectancyfunction'", function() {
   it("calculates male", function() {
     var result = LifeExpectancyMen(0.83,0.89,0.89,0.89,0.89,0.78,0.78);
+    expect(result).toEqual(69.13563233282082);
+  });
+  it("calculates female", function() {
+    var result = LifeExpectancyWomen(0.83,0.89,0.89,0.89,0.89,0.78,0.78);
     console.log(result);
-    expect(result).toEqual(69.78827023241948);
+    expect(result).toEqual(73.66027753476084);
   });
 });
